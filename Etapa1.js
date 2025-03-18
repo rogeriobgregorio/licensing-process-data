ce7ceb9e0779e9f44326ed7b1d157701b1e71798
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-03-18 19:56:06 -0300

    refactor: Refactor Etapa1.js to add exception and error handling

--- Etapa1.js
+++ Etapa1.js
@@ -3,7 +3,8 @@ import { MongoClient } from "mongodb";
 import { ObjectId } from "mongodb";
 
 // Define a URL de conexão com o MongoDB e o nome do banco de dados
-let MONGODB_URL = "mongodb+srv://testeAprova:teste123@cluster0.u2gxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+let MONGODB_URL =
+  "mongodb+srv://testeAprova:teste123@cluster0.u2gxf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 let MONGO_DB = "TesteJs";
 
 // Cria uma nova instância do cliente MongoDB
@@ -23,8 +24,10 @@ async function withMongoDB(action) {
     await DBClient.connect();
     console.log("Conexão com o MongoDB bem-sucedida!");
     await action(DB);
+
   } catch (erro) {
     console.error("Erro ao interagir com o MongoDB:", erro);
+
   } finally {
     await DBClient.close();
     console.log("Conexão com o MongoDB fechada.");
@@ -33,55 +36,83 @@ async function withMongoDB(action) {
 
 // Função para buscar processos no banco de dados
 async function fetchProcesses(db) {
-  return await db
-    .collection("processo")
-    .find(
-      {},
-      {
-        projection: {
-          _id: 0,
-          nP: 1,
-          created_at: 1,
-          "config_metadata.title": 1,
-        },
-      }
-    )
-    .toArray();
+  try {
+    const processes = await db
+      .collection("processo")
+      .find(
+        {},
+        {
+          projection: {
+            _id: 0,
+            nP: 1,
+            created_at: 1,
+            "config_metadata.title": 1,
+          },
+        }
+      )
+      .toArray();
+
+    return processes;
+    
+  } catch (error) {
+    console.error("Erro ao buscar processos no banco de dados:", error);
+    throw new Error("Não foi possível buscar os processos.");
+  }
 }
 
 // Função para salvar processos em JSON
 async function saveAsJson(db) {
-  const listOfProcesses = await fetchProcesses(db);
-  fs.writeFileSync(
-    "listOfProcesses.json",
-    JSON.stringify(listOfProcesses, null, 2)
-  );
-  console.log("Arquivo listOfProcesses.json salvo com sucesso!");
+  try {
+    const listOfProcesses = await fetchProcesses(db);
+
+    if (!listOfProcesses || listOfProcesses.length === 0) {
+      console.error("Nenhum processo encontrado para salvar.");
+      return;
+    }
+
+    fs.writeFileSync(
+      "listOfProcesses.json",
+      JSON.stringify(listOfProcesses, null, 2)
+    );
+
+    console.log("Arquivo listOfProcesses.json salvo com sucesso!");
+  } catch (error) {
+    console.error("Erro ao tentar salvar o arquivo JSON:", error);
+  }
 }
 
 // Função para salvar processos em Excel
 async function saveAsExcel(db) {
-  const listOfProcesses = await fetchProcesses(db);
+  try {
+    const listOfProcesses = await fetchProcesses(db);
 
-  const wb = new ExcelJS.Workbook();
-  const ws = wb.addWorksheet("listaProcessos");
+    if (!listOfProcesses || listOfProcesses.length === 0) {
+      console.error("Nenhum processo encontrado para salvar.");
+      return;
+    }
 
-  ws.columns = [
-    { header: "nP", key: "nP", width: 15 },
-    { header: "Created At", key: "created_at", width: 20 },
-    { header: "Title", key: "title", width: 30 },
-  ];
+    const wb = new ExcelJS.Workbook();
+    const ws = wb.addWorksheet("listaProcessos");
 
-  listOfProcesses.forEach((process) => {
-    ws.addRow({
-      nP: process.nP,
-      created_at: process.created_at,
-      title: process.config_metadata?.title,
+    ws.columns = [
+      { header: "nP", key: "nP", width: 15 },
+      { header: "Created At", key: "created_at", width: 20 },
+      { header: "Title", key: "title", width: 30 },
+    ];
+
+    listOfProcesses.forEach((process) => {
+      ws.addRow({
+        nP: process.nP,
+        created_at: process.created_at,
+        title: process.config_metadata?.title,
+      });
     });
-  });
 
-  await wb.xlsx.writeFile("listaProcessos.xlsx");
-  console.log("Arquivo listaProcessos.xlsx salvo com sucesso!");
+    await wb.xlsx.writeFile("listaProcessos.xlsx");
+    console.log("Arquivo listaProcessos.xlsx salvo com sucesso!");
+  } catch (error) {
+    console.error("Erro ao tentar salvar o arquivo Excel:", error);
+  }
 }
 
 // Executando as funções
