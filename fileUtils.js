308478422db250dd4b96c81965bda69766bf824e
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-03-20 22:04:31 -0300

    feat: Add saveAsExcel function

--- fileUtils.js
+++ fileUtils.js
@@ -1,3 +1,4 @@
+import ExcelJS from "exceljs"; // Importa o módulo para criar e salvar a planilha Excel
 import fs from "fs"; // Importa o módulo de sistema de arquivos
 
 // Função para salvar processos em JSON
@@ -12,8 +13,31 @@ export async function saveAsJson(db, fetchFunction, fileName) {
 
     fs.writeFileSync(fileName, JSON.stringify(listOfProcesses, null, 2));
     console.log(`Arquivo ${fileName} salvo com sucesso!`);
-    
   } catch (error) {
     console.error(`Erro ao tentar salvar o arquivo ${fileName}:`, error);
+  }
+}
+
+// Função para salvar processos em Excel
+export async function saveAsExcel(db, fetchFunction, filename, columns) {
+  try {
+    const listOfProcesses = await fetchFunction(db); 
+
+    if (!listOfProcesses.length) {
+      console.error("Nenhum processo encontrado para salvar.");
+      return;
+    }
+
+    const wb = new ExcelJS.Workbook();
+    const ws = wb.addWorksheet(filename.replace(".xlsx", "")); 
+
+    ws.columns = columns;
+
+    listOfProcesses.forEach((process) => ws.addRow(process));
+
+    await wb.xlsx.writeFile(filename);
+    console.log(`Arquivo ${filename} salvo com sucesso!`);
+  } catch (error) {
+    console.error("Erro ao tentar salvar o arquivo Excel:", error);
   }
 }
